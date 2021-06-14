9997a902eb4d948da436f262498a0c192dc27ab6
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-06-14 06:32:13 +0300

    adds arrays for malenames femealenames and daysoftheweek

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var dob=document.getElementById("DOB").value;
+var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
+var maleNames = ["Kwasi", "Kwadwo",  "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame",];
+var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
+
+
+
+
+
+
+
+
+
+    
+
+
