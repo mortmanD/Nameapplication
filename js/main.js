b399f2046296df3c3d5932019bea9841554933e0
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-06-15 16:32:35 +0300

    edited the form

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
+let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 function nameGenerator(){
-var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
-var maleNames = ["Kwasi", "Kwadwo",  "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame",];
-var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
-var dob=document.getElementById("dob").value;
-var gender=document.getElementById("gender").value;
-if (gender==null || gender==""){  
-    alert("Check your gender");  
-    return false;  
-}
-if (dob==null || dob==""){  
-    alert("Dob cant be blank");  
-    return false;  
+    event.preventDefault();
+    let gender = document.getElementById('gender').value; 
+    let date = document.getElementById('date').value;
+    date = new Date(date);
+
+    if (gender === 'male'){
+        document.getElementById("output").innerHTML =(" Since you were born on " + daysOfWeek[date.getDay()] + ',' + " your Akan name is " + maleNames[date.getDay()]);
+        
+    
+    }
+    else if (gender === 'female'){
+        document.getElementById("output").innerHTML =(" Since you were born on " + daysOfWeek[date.getDay()] + ',' + " your Akan name is " + femaleNames[date.getDay()]);
+       
+    }
+    else{
+        document.getElementById("output").innerHTML =("Error: Ensure you input the date in correct format and the gender is selected");''
+    }
 }
 
-}
