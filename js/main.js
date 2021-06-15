c643ec47df5bb8bb90f2a86ec9e618dbaf64e81f
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-06-15 17:11:30 +0300

    edits the form labels in css

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ function nameGenerator(){
     var date = document.getElementById('date').value;
     date = new Date(date);
     
+    
     if (gender === 'male'&& date !=="null") {
         document.getElementById("output").innerHTML =(" Because your son was born on " + daysOfWeek[date.getDay()] + ',' + " his Akan name should be " + maleNames[date.getDay()]);
-   
     }
     else if (gender === 'female'&& date!=="null" ){
         document.getElementById("output").innerHTML =(" Because your daughter was born on  " + daysOfWeek[date.getDay()] + ',' + " her Akan name should be  " + femaleNames[date.getDay()]);
