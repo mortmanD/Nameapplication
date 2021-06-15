b4ffcf5eeb8e134b1db83ac0b86a0d20f9b37808
MortmanD <dennis.ndonga@student.moringaschool.com>
2021-06-15 08:53:43 +0300

    declares the variable that will be entered on the form in the webpage and assigns them to a placeholder

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ function nameGenerator(){
 var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
 var maleNames = ["Kwasi", "Kwadwo",  "Kwabena", "Kwaku", "Yaw", "Kofi",  "Kwame",];
 var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama",];
+var dob=document.getElementById("dob").value;
+var gender=document.getElementById("gender").value;
 
 
 }
